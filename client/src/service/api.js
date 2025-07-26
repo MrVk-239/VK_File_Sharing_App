77c5278d5963b37a01a0b4d08c0abe5c6297efe7
V Krishnan <vkrishnan2309@gmail.com>
2025-07-26 14:34:03 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000/upload'
 export const uploadfile = async (data) => {
     try {
-        const response = await axios.post('http://localhost:8000/upload', data);
+        const response = await axios.post(backendUrl, data);
         return response.data;
     } catch (error) {
         console.error(error);
