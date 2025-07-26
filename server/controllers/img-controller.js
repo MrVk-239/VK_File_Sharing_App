09075172fab87e7cf169825fec974555c1d13aad
V Krishnan <vkrishnan2309@gmail.com>
2025-07-26 17:04:30 +0530

    added files

--- server/controllers/img-controller.js
+++ server/controllers/img-controller.js
@@ -6,7 +6,7 @@ export const uploadImage=async(req,res)=>{
     }
     try {
         const file = await File.create(fileObj);
-        res.status(200).json({path:`http://localhost:8000/file/${file._id}`, message:"File uploaded successfully",file});
+        res.status(200).json({path:`https://vk-file-sharing-app.onrender.com/file/${file._id}`, message:"File uploaded successfully",file});
     } catch (error) {
         res.status(500).json({error: error.message});
     }
